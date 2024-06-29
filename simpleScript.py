dec2e67adc3f717904415d6a33ca9c2e08362e1d
shubham-sutar <shubhamsutar93@gmail.com>
2024-06-29 20:09:26 +0530

    code added.

--- simpleScript.py
+++ simpleScript.py
@@ -1,2 +1,32 @@
-print("hello world")
-print("Can I Fixed My Problems.")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+from selenium.webdriver.common.by import By
+
+serv_obj = Service("C:\\Drivers\\chromedriver-win32\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://practicetestautomation.com/practice-test-login/")
+driver.maximize_window()
+time.sleep(5)
+
+driver.find_element(By.ID, "username").send_keys("student")
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "input[name=password]").send_keys("Password123")
+time.sleep(2)
+driver.find_element(By.XPATH, "//button[@id='submit']").click()
+time.sleep(2)
+
+act_title = driver.title
+exp_title = "Logged In Successfully | Practice Test Automation"
+
+if act_title == exp_title:
+    print("User Login Successfully.")
+else:
+    print("User Login Failed.")
+time.sleep(3)
+
+driver.close()
+
+
