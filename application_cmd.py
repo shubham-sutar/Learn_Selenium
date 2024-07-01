e33bc720c885316c18d43836c8cdc3060645cc32
shubham-sutar <shubhamsutar93@gmail.com>
2024-07-01 12:26:43 +0530

    code added.

---
+++ application_cmd.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+serv_obj = Service("C:\\Drivers\\chromedriver-win32\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.maximize_window()
+time.sleep(4)
+print("This are the current url of webpage:", driver.current_url)
+
+driver.quit()
