34f56d28dbf830030891a8fa0f233502eb768b06
shubham-sutar <shubhamsutar93@gmail.com>
2024-07-05 14:49:14 +0530

    code added.

--- CheckBox.py
+++ CheckBox.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 serv_obj = Service("C:\\Drivers\\chromedriver-win32\\chromedriver.exe")
 driver = webdriver.Chrome(service=serv_obj)
@@ -10,9 +11,18 @@ driver.maximize_window()
 driver.implicitly_wait(10)
 # time.sleep(5)
 
-
-driver.find_element(By.CSS_SELECTOR, "input.Automation").click()
+# single check
+# driver.find_element(By.CSS_SELECTOR, "input.Automation").click()
 # time.sleep(5)
+
+# multiple checkbox click
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+
+
+for checkbox in checkboxes:
+    checkbox.click()
+
+time.sleep(5)
 
 print("success")
 driver.close()
